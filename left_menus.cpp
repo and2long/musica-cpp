56f0279100c9eca2fde7bb742567cbc6a5300d97
and2long <and2long@icloud.com>
2022-01-26 16:12:00 +0800

    优化左侧菜单。

--- left_menus.cpp
+++ left_menus.cpp
@@ -1,12 +1,19 @@
 #include "left_menus.h"
 #include <QListWidget>
 #include "constants.h"
+#include <QLabel>
 
 LeftMenus::LeftMenus(QWidget *parent)
     : QWidget{parent}
 {
+    QLabel *header = new QLabel("MUSICA音乐", this);
+    header->setFixedSize(200, 100);
+    header->setAlignment(Qt::AlignCenter);
+
+
     QListWidget *items = new QListWidget(this);
+    items->move(0, 100);
     items->setFixedSize(LEFT_MENUS_WIDTH, WINDOW_HEIGHT);
     items->addItems({"发现音乐","下载管理","我的收藏"});
-    items->setSpacing(5);
+    items->setCurrentRow(0);
 }

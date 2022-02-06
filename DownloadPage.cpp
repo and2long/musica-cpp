06733b353af0d45fd8bf325994623fbe6a851046
and2long <and2long@icloud.com>
2022-02-06 11:58:01 +0800

    修改下载页提示文字。

--- DownloadPage.cpp
+++ DownloadPage.cpp
@@ -6,6 +6,7 @@ DownloadPage::DownloadPage(QWidget *parent)
     : QWidget{parent}
 {
     setFixedSize(CONTAINER_WIDTH, CONTAINER_HEIGHT);
-    QLabel *tip = new QLabel("快去下载吧！", this);
+    QLabel *tip = new QLabel("还没有下载的音乐哦，快去下载吧～", this);
+    tip->setFixedSize(CONTAINER_WIDTH, CONTAINER_HEIGHT);
     tip->setAlignment(Qt::AlignCenter);
 }

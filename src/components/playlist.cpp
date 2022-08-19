bd5002b354c1ed2a975b2f5a3df104c468e62bac
Lilong Zhang <and2long@gmail.com>
2022-08-19 13:21:48 +0800

    refactor empty view in playlist.

--- src/components/playlist.cpp
+++ src/components/playlist.cpp
@@ -1,5 +1,6 @@
 #include "playlist.h"
 #include "src/constants.h"
+#include "ClickedLabel.h"
 
 #include <QLabel>
 #include <QStackedLayout>
@@ -7,38 +8,68 @@
 PlayList::PlayList(QWidget *parent)
     : QWidget{parent}
 {
-    auto *container = new QLabel(this);
-    container->setFixedSize(PLAY_LIST_WIDTH, PLAY_LIST_HEIGHT);
-    container->setObjectName("bg");
-    auto *layout = new QStackedLayout();
-    layout->setStackingMode(QStackedLayout::StackingMode::StackOne);
-    container->setLayout(layout);
 
-    auto playListContainer = new QLabel();
-    playListContainer->setFixedSize(PLAY_LIST_WIDTH, PLAY_LIST_HEIGHT);
-    auto *title = new QLabel("当前播放", playListContainer);
+    int headerHeight = 80;
+    auto *headerSize = new QSize(PLAY_LIST_WIDTH, headerHeight);
+    auto *containerSize = new QSize(PLAY_LIST_WIDTH, PLAY_LIST_HEIGHT - headerHeight);
+
+    this->setObjectName("root");
+    auto *rootLayout = new QVBoxLayout();
+    rootLayout->setContentsMargins(0, 0, 0, 0);
+    rootLayout->setSpacing(0);
+    this->setLayout(rootLayout);
+
+    auto header = new QWidget();
+    header->setFixedSize(*headerSize);
+    header->setObjectName("header");
+    rootLayout->addWidget(header);
+
+    auto container = new QWidget();
+    container->setFixedSize(*containerSize);
+    container->setObjectName("container");
+    auto containerLayout = new QStackedLayout();
+    container->setLayout(containerLayout);
+    rootLayout->addWidget(container);
+
+    auto *title = new QLabel("当前播放", header);
     title->setObjectName("title");
     title->setFixedSize(PLAY_LIST_WIDTH, 50);
     title->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
 
-    auto *count = new QLabel("共0首", playListContainer);
+    auto row = new QWidget(header);
+    row->move(0, 50);
+    auto rowLayout = new QHBoxLayout();
+    rowLayout->setContentsMargins(0, 0, 0, 0);
+    rowLayout->setSpacing(0);
+    row->setLayout(rowLayout);
+
+    auto *count = new QLabel("共0首");
+    count->setFixedWidth(PLAY_LIST_WIDTH / 2);
     count->setObjectName("count");
-    count->setFixedSize(PLAY_LIST_WIDTH, 40);
-    count->move(0, 40);
-    count->setAlignment(Qt::AlignLeft | Qt::AlignVCenter);
+    count->setAlignment(Qt::AlignLeft);
 
-    auto *clear = new QLabel("清空列表", playListContainer);
+    auto *clear = new ClickedLabel();
+    clear->setText("清空列表");
+    clear->setFixedWidth(PLAY_LIST_WIDTH / 2);
     clear->setObjectName("clear");
-    clear->setFixedSize(PLAY_LIST_WIDTH, 40);
-    clear->move(0, 40);
-    clear->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
+    clear->setAlignment(Qt::AlignRight);
 
-    auto emptyContainer = new QLabel("你还没有添加任何歌曲！");
-    emptyContainer->setObjectName("emptyContainer");
-    emptyContainer->setFixedSize(PLAY_LIST_WIDTH, PLAY_LIST_HEIGHT);
-    emptyContainer->setAlignment(Qt::AlignCenter);
+    rowLayout->addWidget(count);
+    rowLayout->addWidget(clear);
 
-    layout->addWidget(playListContainer);
-    layout->addWidget(emptyContainer);
-    layout->setCurrentIndex(1);
+    auto emptyView = new QWidget();
+    emptyView->setFixedSize(*containerSize);
+    auto *tipTitle = new QLabel("你还没有添加任何歌曲！", emptyView);
+    auto *tipSubtitle = new ClickedLabel(emptyView);
+    tipSubtitle->setText("去首页发现音乐");
+    tipTitle->setObjectName("tipTitle");
+    tipSubtitle->setObjectName("tipSubtitle");
+    tipTitle->setFixedWidth(PLAY_LIST_WIDTH);
+    tipSubtitle->setFixedWidth(PLAY_LIST_WIDTH);
+    tipTitle->setAlignment(Qt::AlignHCenter);
+    tipSubtitle->setAlignment(Qt::AlignHCenter);
+    tipTitle->move(0, 100);
+    tipSubtitle->move(0, 130);
+
+    containerLayout->addWidget(emptyView);
 }
